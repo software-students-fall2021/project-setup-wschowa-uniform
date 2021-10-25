0ba2fa9e9b40e5c81242a7d512301a6fa48ad705
Laura Lou <wl1877@nyu.edu>
2021-10-25 18:07:02 -0400

    finished detailed post page

---
+++ front-end/src/components/Comments.jsx
@@ -0,0 +1,47 @@
+import React, { useState, createElement } from 'react';
+import {Comment, Tooltip, Avatar, Form, Input, Button} from 'antd';
+import moment from 'moment';
+import { LikeFilled } from '@ant-design/icons';
+
+function Comments() {
+    const [likes, setLikes] = useState(0);
+    const [action, setAction] = useState(null);
+
+    const like = () => {
+        setLikes(likes + 1);
+        setAction('liked');
+    };
+
+    const reply = () => {
+        setAction('reply');
+    }
+
+    const actions = [
+        <span key="comment-basic-like" onClick={like}>Like</span>,
+        <span key="comment-basic-reply-to">Reply to</span>,
+        <span key="comment-basic-share">Share</span>,
+        <span key="comment-basic-like-icon" className="comment-like-icon"><LikeFilled/>{likes}</span>
+    ];
+
+    return (
+        <Comment
+            actions={actions}
+            author={<a>Han Solo</a>}
+            avatar={<Avatar src="https://joeschmoe.io/api/v1/random" alt="Han Solo" />}
+            content={
+                <p>
+
+                </p>
+            }
+            datetime={
+                <Tooltip title={moment().format('YYYY-MM-DD HH:mm:ss')}>
+                    <span>{moment().fromNow()}</span>
+                </Tooltip>
+            }
+        />
+
+    );
+
+}
+
+export default Comments;
---
+++ front-end/src/components/DetailedPost.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import Comments from "./Comments"
+import {Card, Avatar, Comment, Button, Form, Input} from 'antd';
+import {HeartOutlined, CommentOutlined, SendOutlined} from '@ant-design/icons';
+import "./DetailedPost.css"
+
+function DetailedPost() {
+    const onFinish = (values) => {
+        console.log("the comment is:", values);
+        const {newComment} = values;
+        // temporary there is no database to record the new comment
+    }
+    // get the post details from database
+    const getPost = () => {
+    }
+
+    return (
+        <div className="detailed-post-page">
+            <Card
+                title={<h2 className="playlist-name"></h2>}
+                cover={<img className="detailed-post-album"
+                            src="https://picsum.photos/200"/>}
+            >
+
+                <Card.Grid className="detailed-post-response" hoverable={false}>
+                    <span className="detailed-post-like">
+                        <Button type="text"
+                                icon={<HeartOutlined className="detailed-post-like-icon"/>}/>
+                        </span>
+                    <span className="detailed-post-com">
+                        <Button type="text"
+                                icon={<CommentOutlined className="detailed-post-com-icon"/>}/>
+                    </span>
+                    <span className="detailed-post-send">
+                        <Button type="text"
+                                icon={<SendOutlined className="detailed-post-send-icon"/>}/>
+                    </span>
+                </Card.Grid>
+
+                <Card.Grid className="detailed-post-comments" hoverable={false}>
+                    <Comment
+                        avatar={<Avatar src="https://joeschmoe.io/api/v1/random" alt="Han Solo"/>}
+                        content={<Form className="detailed-post-leave-comment">
+                            <Form.Item>
+                                <Input
+                                    placeholder="Leave your comment..."
+                                />
+                            </Form.Item>
+                            <Form.Item>
+                                <Button type="primary" htmlType="submit">Submit</Button>
+                            </Form.Item>
+                        </Form>}
+                    />
+                    <Comments />
+                    <Comments />
+                </Card.Grid>
+            </Card>
+        </div>
+    )
+
+}
+
+export default DetailedPost;
