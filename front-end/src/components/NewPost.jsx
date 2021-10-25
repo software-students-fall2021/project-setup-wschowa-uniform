97aee411703815b0e2e2238ea6e13f3cdc0737f4
Sylv <79240358+Sylvia-Siyu-Chen@users.noreply.github.com>
2021-10-25 15:39:36 -0400

    Add "create new post" page

---
+++ front-end/src/components/NewPost.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./NewPost.css";
+
+const NewPost = (props) => {
+        return(
+            <div>
+                <h1>Spotify Music Sharing</h1>
+                <div className="newPost">
+                    <div><input name="playlistName" className="nameInput" type="text" placeholder="Playlist Name Here ..."/></div>
+                    <div><input name="playlistLink" className="linkInput" type="url" placeholder="Pasta Playlist Link Here"/></div>
+                    <div className = "spotifyPlaylist">spotify showcase will go here</div>
+                    <div><input name="caption" className = "captionInput" type="text" placeholder="Write a Caption ..."/></div>
+                </div>
+            </div>
+        )
+}
+
+export default NewPost
+
+
