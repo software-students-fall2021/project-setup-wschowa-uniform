80c8e9418aba16431512234632f01121aa0248b2
Mingqian Sun <47837369+DanielS511@users.noreply.github.com>
2021-10-26 11:49:26 -0400

    Prevent link in header opening a new tab

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,42 +1,44 @@
-import React from 'react';
-import { MenuFoldOutlined } from '@ant-design/icons';
-import { Menu, Dropdown, Button } from 'antd';
+import React from "react"
+import { MenuFoldOutlined } from "@ant-design/icons"
+import { Menu, Dropdown, Button } from "antd"
 import "./Header.css"
 
 function Header() {
-
-    const menu = (
-        <Menu>
-            <Menu.Item>
-                <a target="_blank" rel="noopener noreferrer" href="">
-                    Home
-                </a>
-            </Menu.Item>
-            <Menu.Item>
-                <a target="_blank" rel="noopener noreferrer" href="">
-                    Post
-                </a>
-            </Menu.Item>
-            <Menu.Item>
-                <a target="_blank" rel="noopener noreferrer" href="">
-                    Profile
-                </a>
-            </Menu.Item>
-            <Menu.Item>
-                <a target="_blank" rel="noopener noreferrer" href="">
-                    Sign out
-                </a>
-            </Menu.Item>
-        </Menu>
-    );
-    return (
-        <header className="App-header">
-            <Dropdown overlay={menu} placement="bottomCenter" arrow>
-                <Button className="dropdown-button" icon={<MenuFoldOutlined className="menu-icon"/>}></Button>
-            </Dropdown>
-            <span className="App-title">Spotify Music Sharing</span>
-        </header>
-    );
+	const menu = (
+		<Menu>
+			<Menu.Item>
+				<a rel="noopener noreferrer" href="/">
+					Home
+				</a>
+			</Menu.Item>
+			<Menu.Item>
+				<a rel="noopener noreferrer" href="/newpost">
+					Post
+				</a>
+			</Menu.Item>
+			<Menu.Item>
+				<a rel="noopener noreferrer" href="">
+					Profile
+				</a>
+			</Menu.Item>
+			<Menu.Item>
+				<a rel="noopener noreferrer" href="/login">
+					Login/Sign out
+				</a>
+			</Menu.Item>
+		</Menu>
+	)
+	return (
+		<header className="App-header">
+			<Dropdown overlay={menu} placement="bottomCenter" arrow>
+				<Button
+					className="dropdown-button"
+					icon={<MenuFoldOutlined className="menu-icon" />}
+				></Button>
+			</Dropdown>
+			<span className="App-title">Spotify Music Sharing</span>
+		</header>
+	)
 }
 
-export default Header;
+export default Header
