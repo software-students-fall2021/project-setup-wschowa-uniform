2ff6365c1d70ebc35510538e57e7229870e5989c
Mingqian Sun <47837369+DanielS511@users.noreply.github.com>
2021-10-26 23:09:51 -0400

    Syntax fix

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,42 +6,41 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
 import Login from "./components/Login"
 import Signup from "./components/Signup"
 import NewPost from "./components/NewPost"
-import DetailedPost from "./components/DetailedPost";
-import Header from "./components/Header";
+import DetailedPost from "./components/DetailedPost"
+import Header from "./components/Header"
 import Profile from "./components/Profile"
-import EditProfile from "./componenets/EditProfile"
-
+import EditProfile from "./components/EditProfile"
 
 function App() {
-  return (
-      <div>
-        <Header/>
-        <Router>
-          <Container fluid="md">
-            <Switch>
-              <Route path="/login">
-                <Login/>
-              </Route>
-              <Route path="/signup">
-                <Signup/>
-              </Route>
-              <Route path="/newpost">
-                <NewPost/>
-              </Route>
-              <Route path="/seepost">
-                <DetailedPost/>
-              </Route>
-              <Route path="/profile">
-                <Profile />
-              </Route>
-              <Route path="/editprofile">
-                <EditProfile />
-              </Route>
-            </Switch>
-          </Container>
-        </Router>
-      </div>
-  )
+	return (
+		<div>
+			<Header />
+			<Router>
+				<Container fluid="md">
+					<Switch>
+						<Route path="/login">
+							<Login />
+						</Route>
+						<Route path="/signup">
+							<Signup />
+						</Route>
+						<Route path="/newpost">
+							<NewPost />
+						</Route>
+						<Route path="/seepost">
+							<DetailedPost />
+						</Route>
+						<Route path="/profile">
+							<Profile />
+						</Route>
+						<Route path="/editprofile">
+							<EditProfile />
+						</Route>
+					</Switch>
+				</Container>
+			</Router>
+		</div>
+	)
 }
 
 export default App
--- front-end/src/components/Login.jsx
+++ front-end/src/components/Login.jsx
@@ -1,23 +1,23 @@
 import { Button, Image } from "react-bootstrap"
 import "./Login.css"
-import logo from "../image/logo.jpg"
+import logo from "../image/Logo.jpg"
 function Login() {
-  return (
-    <div className="login">
-      <Image className="login_logo" src={logo} alt="Logo" roundedCircle />
-      <input className="login_input" type="text" placeholder="Username"></input>
-      <input
-        className="login_input"
-        type="password"
-        placeholder="Password"
-      ></input>
-      <Button className="login_button" variant="outline-primary" href="/">
-        Login
-      </Button>{" "}
-      <div className="login_signup">
-        <a href="/signup">Don't have an account?</a>
-      </div>
-    </div>
-  )
+	return (
+		<div className="login">
+			<Image className="login_logo" src={logo} alt="Logo" roundedCircle />
+			<input className="login_input" type="text" placeholder="Username"></input>
+			<input
+				className="login_input"
+				type="password"
+				placeholder="Password"
+			></input>
+			<Button className="login_button" variant="outline-primary" href="/">
+				Login
+			</Button>{" "}
+			<div className="login_signup">
+				<a href="/signup">Don't have an account?</a>
+			</div>
+		</div>
+	)
 }
 export default Login
