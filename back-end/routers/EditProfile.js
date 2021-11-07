7edb933996745d85b635fed442d846b71e75b00c
Mingqian Sun <47837369+DanielS511@users.noreply.github.com>
2021-11-06 20:07:14 -0400

    Add post for EditProfile

--- back-end/routers/EditProfile.js
+++ back-end/routers/EditProfile.js
@@ -3,16 +3,24 @@ const express = require("express")
 const router = express.Router()
 const DATA_URL = "https://my.api.mockaroo.com/user.json?key=99391580"
 
-function getData(req, res, next) {
-	console.log("get data")
-}
+/*
+A middleware that console out the request original url
+*/
 function logger(req, res, next) {
 	console.log(req.originalUrl)
 	next()
 }
+/*
+The get method of the api
+req: the request should be a url with the form of "/editprofile?id={user id}"
+res: the response contains a json object of a user with {user id}
+*/
 router.get("/", logger, (req, res, next) => {
+	// get the user id from the req url
 	const user_id = req.query.id
 	console.log(user_id)
+	console.log(`The id is ${req.id}`)
+	// get data from database
 	axios
 		.get(DATA_URL)
 		.then((apiresponse) => {
@@ -21,8 +29,18 @@ router.get("/", logger, (req, res, next) => {
 		})
 		.catch((err) => next(err))
 })
+/*
+Post method the change user profile with the {user id}
+req: the request should be a url with the form of "/editprofile?id={user id}"
+*/
 router.post("/", logger, (req, res) => {
-	res.send("hhhhhhh")
+	const user_id = req.query.id
+	console.log(req.body)
+	post_name = req.body.name
+	post_age = req.body.age
+	post_gender = req.body.gender
+	post_description = req.body.description
+	res.send("complete")
 })
 
 module.exports = router
