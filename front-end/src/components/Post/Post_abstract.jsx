e4bb7882d4046a9ff2f64b506e8334d551b5cef7
Mingqian Sun <47837369+DanielS511@users.noreply.github.com>
2021-11-11 11:21:45 -0500

    Update profile page UI

--- front-end/src/components/Post/Post_abstract.jsx
+++ front-end/src/components/Post/Post_abstract.jsx
@@ -1,14 +1,14 @@
 import "./Post_abstract.css"
 import { Card, ListGroup, ListGroupItem } from "react-bootstrap"
-import {useHistory} from "react-router-dom";
+import { useHistory } from "react-router-dom"
 
 function Post_abstract(props) {
-	const history = useHistory();
+	const history = useHistory()
 	const redirect = () => {
-		history.push(`/seepost/${props.details.id}`);
+		history.push(`/seepost/${props.details.id}`)
 	}
 	return (
-		<Card style={{ width: "18rem" }} onClick={redirect}>
+		<Card style={{ width: "30rem" }} onClick={redirect}>
 			<Card.Body>
 				<Card.Title>{props.details.name}</Card.Title>
 				<Card.Text>{props.details.description}</Card.Text>
--- front-end/src/components/Profile.jsx
+++ front-end/src/components/Profile.jsx
@@ -7,38 +7,38 @@ import Post_abstract from "./Post/Post_abstract"
 import "./Profile.css"
 
 const Profile = () => {
-    const [edit,showEdit] = React.useState(false)
-    const [butname, editButName] = React.useState('Edit Profile')
-    const[file, selectedFile] = React.useState(null)
+	const [edit, showEdit] = React.useState(false)
+	const [butname, editButName] = React.useState("Edit Profile")
+	const [file, selectedFile] = React.useState(null)
+	const [data, setData] = useState([])
 
-    const fileSelectedHandler = event =>{
-        selectedFile(event.target.files[0])
-    }
+	const fileSelectedHandler = (event) => {
+		selectedFile(event.target.files[0])
+	}
 
-    const toggleEdit= () =>{
-        showEdit(!edit)
-        console.log(edit)
-    }
+	const toggleEdit = () => {
+		showEdit(!edit)
+		console.log(edit)
+	}
 
-    const buttonName = () =>{
-        if (edit===false){
-            editButName("Save Changes")
-        }
-        else{
-            editButName("Edit Profile")
-        }
-    }
+	const buttonName = () => {
+		if (edit === false) {
+			editButName("Save Changes")
+		} else {
+			editButName("Edit Profile")
+		}
+	}
 
-    const overall = () =>{
-        toggleEdit()
-        buttonName()
-    }
+	const overall = () => {
+		toggleEdit()
+		buttonName()
+	}
 
-    const [firstname,setFirstName] = React.useState('First Name Here')
-    const [lastname,setLastName] = React.useState('Last Name Here')
-    const [age,setAge] = React.useState('Age Here')
-    const [gender,setGender] = React.useState('Gender Here')
-    const [desc,setDesc] = React.useState('Description Here')
+	const [firstname, setFirstName] = React.useState("First Name Here")
+	const [lastname, setLastName] = React.useState("Last Name Here")
+	const [age, setAge] = React.useState("Age Here")
+	const [gender, setGender] = React.useState("Gender Here")
+	const [desc, setDesc] = React.useState("Description Here")
 
 	useEffect(() => {
 		async function fetchData() {
@@ -49,76 +49,82 @@ const Profile = () => {
 		}
 		fetchData()
 	}, [])
-	
+
 	return (
-        <section>
-            <div className='Profile'>
-            <h1>Profile</h1>
-            <div>
-            <img src="https://picsum.photos/200"/></div>
-            <p>
-            Name: {firstname} + '' + {lastname}
-            </p>
-            <p>
-            Age: {age}
-            </p>
-            <p>
-            Gender: {gender}
-            </p>
-            <p>
-            {desc}
-            </p>
-            </div>
-        <button onClick={overall}>{butname}</button>
-            <p className={edit ? '':'hidden'}>
-            <form className='first_name'>
-                <label>Enter your first name:
-                    <input
-                        type="text" 
-                        value={firstname}
-                        onChange={(e) => setFirstName(e.target.value)}
-                    />
-                </label>
-            </form>
-            <form className='last_name'>
-                <label>Enter your last name:
-                    <input
-                        type="text" 
-                        value={lastname}
-                        onChange={(e) => setLastName(e.target.value)}
-                    />
-                </label>
-            </form>
-            <form className='age'>
-                <label>Enter your age:
-                    <input
-                        type="text" 
-                        value={age}
-                        onChange={(e) => setAge(e.target.value)}
-                    />
-                </label>
-            </form>
-            <form className='gender'>
-                <label>Enter your gender:
-                    <input
-                        type="text" 
-                        value={gender}
-                        onChange={(e) => setGender(e.target.value)}
-                    />
-                </label>
-            </form>
-            <form className='description'>
-                <label>Enter your biograpghy:
-                    <input
-                        type="text" 
-                        value={desc}
-                        onChange={(e) => setDesc(e.target.value)}
-                    />
-                </label>
-            </form>
-            </p>
-        </section>
-    )
+		<section>
+			<div className="Profile">
+				<h1>Profile</h1>
+				<div>
+					<img src="https://picsum.photos/200" />
+				</div>
+				<p>
+					Name: {firstname} + '' + {lastname}
+				</p>
+				<p>Age: {age}</p>
+				<p>Gender: {gender}</p>
+				<p>{desc}</p>
+			</div>
+			<button onClick={overall}>{butname}</button>
+			<p className={edit ? "" : "hidden"}>
+				<form className="first_name">
+					<label>
+						Enter your first name:
+						<input
+							type="text"
+							value={firstname}
+							onChange={(e) => setFirstName(e.target.value)}
+						/>
+					</label>
+				</form>
+				<form className="last_name">
+					<label>
+						Enter your last name:
+						<input
+							type="text"
+							value={lastname}
+							onChange={(e) => setLastName(e.target.value)}
+						/>
+					</label>
+				</form>
+				<form className="age">
+					<label>
+						Enter your age:
+						<input
+							type="text"
+							value={age}
+							onChange={(e) => setAge(e.target.value)}
+						/>
+					</label>
+				</form>
+				<form className="gender">
+					<label>
+						Enter your gender:
+						<input
+							type="text"
+							value={gender}
+							onChange={(e) => setGender(e.target.value)}
+						/>
+					</label>
+				</form>
+				<form className="description">
+					<label>
+						Enter your biograpghy:
+						<input
+							type="text"
+							value={desc}
+							onChange={(e) => setDesc(e.target.value)}
+						/>
+					</label>
+				</form>
+			</p>
+			<h2 id="previous_post_header">Previous Posts</h2>
+			<section className="posts">
+				{data.map((item) => (
+					<Post_abstract className="post" key={item.id} details={item} />
+				))}
+			</section>
+		</section>
+	)
 }
 
 export default Profile
