c91050a0f83eaa1b7cfa7d90b81c6c0e5f573919
Mingqian Sun <47837369+DanielS511@users.noreply.github.com>
2021-11-22 18:45:40 -0500

    Modify database

--- back-end/db.js
+++ back-end/db.js
@@ -1,62 +1,58 @@
-import mongoose from 'mongoose';
-import URLSlugs from 'mongoose-url-slugs';
-
-
+// import  URLSlugs  from "mongoose-url-slugs" //Error: Cannot use import statement outside a module
+const mongoose = require("mongoose")
+require("dotenv").config()
+const Schema = mongoose.Schema
+// Try to connect to atlas
+const uri = process.env.MONGODB_URI
 // is the environment variable, NODE_ENV, set to PRODUCTION
-let dbconf;
-if (process.env.NODE_ENV === 'PRODUCTION') {
- // if we're in PRODUCTION mode, then read the configration from a file
- // use blocking file io to do this...
- const fs = require('fs');
- // import path from "path"
- const path = require('path');
- const fn = path.join(__dirname, 'config.json');
- const data = fs.readFileSync(fn);
+// let dbconf
+// if (process.env.NODE_ENV === "PRODUCTION") {
+// 	// if we're in PRODUCTION mode, then read the configration from a file
+// 	// use blocking file io to do this...
+// 	const fs = require("fs")
+// 	// import path from "path"
+// 	const path = require("path")
+// 	const fn = path.join(__dirname, "config.json")
+// 	const data = fs.readFileSync(fn)
 
- const conf = JSON.parse(data);
- dbconf = conf.dbconf;
-} else {
- dbconf = '';
-}
+// 	const conf = JSON.parse(data)
+// 	dbconf = conf.dbconf
+// } else {
+// 	dbconf = ""
+// }
+// // users
+// // * authentication required
+// // * so users have a username and password
+// const User = new Schema({
+//     username : {type: String, require : true},
+//     password : {type: String, require : true, min: 18},
+//     gender : {type: String, enum:['male','female','prefer not to tell']},
+//     // posts : [Post]
+// },{timestamps : true})
 
-// users
-// * authentication required
-// * so users have a username and password
-const User = new Schema({
-    username : {type: String, require : true},
-    password : {type: String, require : true, min: 18},
-    gender : {type: String, enum:['male','female','prefer not to tell']},
-    // posts : [Post]
-},{timestamps : true})
+// const Comment = new Schema({
+//     content : {type : String, require: true, max:65},
+//     user : User,
+//     isReply : Boolean,
+//     replyTo : Comment,
+// })
 
+// // Post
+// // * each Post must have a related user,
+// // * they also can have 0 or more comments
 
+// const Post = new Schema({
+//   user: {type: User,require: true},
+//   playlist_name : {type: String, required: true},
+//   playlist_link : {type:String, require: true},
+//   caption: {type: String, required: true},
+//   comments : [Comment]
+// })
 
-const Comment = new Schema({
-    content : {type : String, require: true, max:65},
-    user : User,
-    isReply : Boolean,
-    replyTo : Comment,  
-})
+// mongoose.model('User',User);
+// mongoose.model('Post',Post);
+// mongoose.model('Comment',Comment);
 
-// Post
-// * each Post must have a related user,
-// * they also can have 0 or more comments
-
-const Post = new Schema({
-  user: {type: User,require: true},
-  playlist_name : {type: String, required: true},
-  playlist_link : {type:String, require: true},
-  caption: {type: String, required: true},
-  comments : [Comment]
-})
-
-
-mongoose.model('User',User);
-mongoose.model('Post',Post);
-mongoose.model('Comment',Comment);
-
-mongoose.connect(dbconf);
-
-
-
-
+// mongoose.connect(dbconf);
+const connection = mongoose.createConnection(uri)
+module.exports = connection
---
+++ back-end/models/Post.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose")
+const connection = require("../db")
+// users
+// * authentication required
+// * so users have a username and password
+const user = new mongoose.Schema(
+	{
+		username: { type: String, require: true },
+		password: { type: String, require: true },
+		gender: { type: String, enum: ["male", "female", "prefer not to tell"] },
+		description: { type: String, required: true },
+		// posts: [post],
+	},
+	{ timestamps: true }
+)
+const comment = new mongoose.Schema({
+	content: { type: String, require: true, max: 65 },
+	user: { type: user, require: true },
+	isReply: Boolean,
+	replyTo: [comment], //Error: Cannot access 'Comment' before initialization
+})
+// Post
+// * each Post must have a related user,
+// * they also can have 0 or more comments
+const post = new mongoose.Schema({
+	user: { type: user, require: true },
+	playlist_name: { type: String, required: true },
+	playlist_link: { type: String, require: true },
+	caption: { type: String, required: true },
+	comments: [comment],
+})
+const Post = connection.model("Post", post)
+module.exports = Post
