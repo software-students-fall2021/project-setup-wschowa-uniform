221d315a5f70cf97d5eb7ef89fb15c1b37527a4b
Mingqian Sun <47837369+DanielS511@users.noreply.github.com>
2021-11-22 21:49:48 -0500

    Add login backend

--- back-end/routers/Login.js
+++ back-end/routers/Login.js
@@ -3,9 +3,11 @@ const router = express.Router()
 const cors = require("cors") // middleware for enabling CORS (Cross-Origin Resource Sharing) requests.
 const morgan = require("morgan") // middleware for nice logging of incoming HTTP requests
 require("dotenv").config({ silent: true }) // load environmental variables from a hidden file named .env
+const User = require("../models/User")
+const bcrypt = require("bcrypt")
 
 // the following are used for authentication with JSON Web Tokens
-const _ = require("lodash") // the lodash module has some convenience functions for arrays that we use to sift through our mock user data... you don't need this if using a real database with user info
+// const _ = require("lodash") // the lodash module has some convenience functions for arrays that we use to sift through our mock user data
 
 const jwt = require("jsonwebtoken")
 const passport = require("passport")
@@ -32,11 +34,11 @@ router.get("/", (req, res) => {
 	res.status(200).send("complete")
 })
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
 	// brab the name and password that were submitted as POST body data
 	const username = req.body.username
 	const password = req.body.password
-	// console.log(`${username}, ${password}`)
+	console.log(`${username}, ${password}`)
 	if (!username || !password) {
 		// no username or password received in the POST body... send an error
 		res
@@ -44,26 +46,31 @@ router.post("/", (req, res) => {
 			.json({ success: false, message: `no username or password supplied.` })
 	}
 
-	// usually this would be a database call, but here we look for a matching user in our mock data
-	const user = users[_.findIndex(users, { username: username })]
+	// const user = users[_.findIndex(users, { username: username })] //mock data legacy
+	const user = await User.findOne({ username: username })
+	console.log(user)
+
 	if (!user) {
 		// no user found with this name... send an error
 		res
 			.status(401)
 			.json({ success: false, message: `user not found: ${username}.` })
-	}
-
-	// assuming we found the user, check the password is correct
-	// we would normally encrypt the password the user submitted to check it against an encrypted copy of the user's password we keep in the database... but here we just compare two plain text versions for simplicity
-	else if (req.body.password == user.password) {
-		// the password the user entered matches the password in our "database" (mock data in this case)
-		// from now on we'll identify the user by the id and the id is the only personalized value that goes into our token
-		const payload = { id: user.id } // some data we'll encode into the token
-		const token = jwt.sign(payload, jwtOptions.secretOrKey) // create a signed token
-		res.json({ success: true, username: user.username, token: token }) // send the token to the client to store
 	} else {
-		// the password did not match
-		res.status(401).json({ success: false, message: "passwords did not match" })
+		// If we have our user, we want to compare the password user entered to the one in the database
+		bcrypt.compare(req.body.password, user.password, function (err, response) {
+			if (err) {
+				res.status(401).send("error: fail to compare passwords")
+			}
+			if (response) {
+				const payload = { id: user.id } // some data we'll encode into the token
+				const token = jwt.sign(payload, jwtOptions.secretOrKey) // create a signed token
+				res.json({ success: true, username: user.username, token: token }) // send the token to the client to store
+			} else {
+				res
+					.status(401)
+					.json({ success: false, message: "passwords did not match" })
+			}
+		})
 	}
 })
 
