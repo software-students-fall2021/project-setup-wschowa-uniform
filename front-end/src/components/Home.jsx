04a3bca0fdf692105da82362c7d6e9471d6b0e0d
Mingqian Sun <47837369+DanielS511@users.noreply.github.com>
2021-11-30 12:31:15 -0500

    Complete Home Page

--- front-end/src/components/Home.jsx
+++ front-end/src/components/Home.jsx
@@ -1,14 +1,29 @@
 import "./Home.css"
-import SearchBar from './SearchBar';
-import HomePagePost from './HomePagePost';
+import SearchBar from "./SearchBar"
+import HomePagePost from "./HomePagePost"
+import axios from "axios"
+import { useState, useEffect, React } from "react"
 function Home() {
-  return (
-    <div>
-    <SearchBar/>
-    <HomePagePost/>
-    <HomePagePost/>
-    <HomePagePost/>
-   </div> 
-  );
+	const [posts, setPosts] = useState([])
+	useEffect(() => {
+		async function fetchPosts() {
+			console.log("fetch")
+			//fetch the past posts data
+			const result = await axios("/home")
+			setPosts(result.data)
+			console.log(posts)
+		}
+		fetchPosts()
+	}, [])
+	return (
+		<div>
+			{/* <SearchBar/> */}
+			<section>
+				{posts.map((item) => (
+					<HomePagePost key={item.id} details={item} />
+				))}
+			</section>
+		</div>
+	)
 }
 export default Home
--- front-end/src/components/HomePagePost.jsx
+++ front-end/src/components/HomePagePost.jsx
@@ -1,25 +1,30 @@
-
 import "./HomePagePost.css"
-function HomePagePost() {
-  return (
-    <div className="post_container">
-        <div className="post_content">
-            <div className="profile_pic_wrapper">
-            <img src="" className="profile_pic" ></img>
-            </div>
-                <div className="playlist_title">
-                    Playlist Name
-                </div>
-                
-                <div className="playlist_username">
-                    posted by User
-                </div>
 
-                <div className="playlist_embed">
-                <iframe className="playlist_iframe" src="https://open.spotify.com/embed/playlist/37i9dQZF1DWWjGdmeTyeJ6" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
-                </div>
-        </div>
-    </div>
-);
+function HomePagePost(props) {
+	const uri =
+		"https://open.spotify.com/embed/playlist/" + props.details.playlist_link
+	return (
+		<div className="post_container">
+			<div className="post_content">
+				{/* <div className="profile_pic_wrapper">
+            <img src="" className="profile_pic" ></img>
+            </div> 
+            This is the pic which may be completed later...*/}
+				<div className="playlist_title">{props.details.playlist_name}</div>
+
+				<div className="playlist_username">{props.details.user.username}</div>
+
+				<div className="playlist_embed">
+					<iframe
+						className="playlist_iframe"
+						src={uri}
+						frameBorder="0"
+						allowfullscreen=""
+						allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+					></iframe>
+				</div>
+			</div>
+		</div>
+	)
 }
 export default HomePagePost
